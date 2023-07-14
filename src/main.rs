4464b479f809aba846ee51266e63d731e103fc44
ayushigarg2 <aggarwalayush251@gmail.com>
2023-07-14 15:58:35 +0530

    Add random data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate image;
+use image::{RgbImage, Rgb};
+
+extern crate rand;
+use rand::Rng;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let  rows = 1080;
+    let  columns = 1920;
+
+    let mut img = RgbImage::new(columns, rows);
+
+    let mut bitstream:Vec<u8> = Vec::new();
+    let mut rng = rand::thread_rng();
+
+    let capacity = rows*columns*3;
+
+
+    for i in 0..capacity as usize {
+        bitstream.push(rng.gen::<u8>());
+        println!("{}", bitstream[i]);
+    }
+
+
+    let _ = img.save("hello.png");
+
 }
