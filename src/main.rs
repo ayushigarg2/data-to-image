bfd636ee2038d9cd87b3c88e74e0d38a46963856
namanlp <naman@garudalinux.org>
2023-08-11 02:46:26 +0530

    Done Generating image

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,46 @@
 extern crate image;
-use image::{RgbImage, Rgb, ImageBuffer, Luma};
 
-extern crate rand;
-use rand::Rng;
+use image::{ImageBuffer, Luma, GrayImage};
 
 // We will Create an image, 1080x1920
 // Each pixel is set to either black or white
 // Black Represents 0 and white represents 1
 
 
+pub fn convert_to_image(data:&Vec<bool>, columns:u32, rows:u32) -> ImageBuffer<Luma<u8>, Vec<u8>>{
+
+    // Create an empty image of columns×rows size
+
+    let mut image = GrayImage::new(columns, rows);
+
+    for i in 0..data.len() {
+
+        // If bit is 1, set color to white
+        if data[i] == true {
+            image.get_pixel_mut(i as u32/columns, i as u32%rows).0 = [255];
+        }
+
+        // Set it to 0 otherwise
+        else {
+            image.get_pixel_mut(i as u32/columns, i as u32%rows).0 = [0];
+        }
+    }
+
+    return image
+}
+
 fn main() {
 
     let  rows = 1080;
     let  columns = 1920;
 
-    let mut img = RgbImage::new(columns, rows);
+    // Generate Sample Data
+    let mut sample_data = vec![false; 2073600];
 
-    let mut bitstream:Vec<u8> = Vec::new();
-    let mut rng = rand::thread_rng();
-
-    let capacity = rows*columns*3;
-
-
-    for i in 0..capacity as usize {
-        bitstream.push(rng.gen::<u8>());
-        println!("{}", bitstream[i]);
+    for i in (0..2073600 as usize).step_by(123) {
+        sample_data[i] = true
     }
 
-
-    let _ = img.save("hello.png");
+    let _ = convert_to_image(&sample_data, columns, rows).save("hello.png");
 
 }
